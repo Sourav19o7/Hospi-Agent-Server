7c11402642a181215a422af06b8372c0dab8a13a
shettydevesh <deveshshetty66@gmail.com>
2025-05-13 16:26:15 +0530

    insights bug fixes

--- controllers/insightController.js
+++ controllers/insightController.js
@@ -3,6 +3,9 @@ const asyncHandler = require('express-async-handler');
 const { supabaseAdmin } = require('../config/supabase');
 const {
   generateRevenueInsights,
+  generatePatientInsights,
+  generateInventoryInsights,
+  generateSchedulingInsights,
 } = require("../services/insightGenerationService");
 
 /**
